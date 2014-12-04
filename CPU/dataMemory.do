a6cddb9548558425582b7644e64dc19c461af36f
Ben Kahle <ben@kahle.org>
2014-12-03 20:40:19 -0500

    fix dataMemory testing.

--- CPU/dataMemory.do
+++ CPU/dataMemory.do
@@ -1,13 +1,5 @@
 vlog -reportprogress 300 -work work dataMemory.v
-vsim -voptargs="+acc" testBenchHarness
-
-add wave -position insertpoint  \
-sim:/testBenchHarness/clk \
-sim:/testBenchHarness/regWE \
-sim:/testBenchHarness/addr \
-sim:/testBenchHarness/dataIn \
-sim:/testBenchHarness/dataOut \
-sim:/testBenchHarness/beginTest 
+vsim -voptargs="+acc" testBench
 
 run -all
 
--- CPU/dataMemory.v
+++ CPU/dataMemory.v
@@ -12,69 +12,53 @@ module dataMemory(clk, regWE, addr, dataIn, dataOut);
   end
   assign dataOut = mem[addr];
 endmodule
-module testBenchHarness;
-  wire clk, regWE;
-  wire[9:0] addr;
-  wire[31:0] dataIn;
+
+module testBench;
+  reg clk;
+  reg regWE;
+  reg[9:0] addr;
+  reg[31:0] dataIn;
   wire[31:0] dataOut;
-  reg beginTest;
+  reg failed;
 
   dataMemory memTest(clk, regWE, addr, dataIn, dataOut);
-  testBench test(clk, beginTest, regWE, addr, dataIn, dataOut);
 
   initial begin
-    beginTest = 0;
-    #10;
-    beginTest = 1;
-    #300;
-  end
-endmodule
-module testBench(clk, beginTest, regWE, addr, dataIn, dataOut);
-  output reg clk;
-  output reg regWE;
-  output reg[9:0] addr;
-  output reg[31:0] dataIn;
-  input[31:0] dataOut;
-  input beginTest;
-
-  initial begin
+    failed = 0;
     clk = 0;
     regWE = 0;
     addr = 0;
     dataIn = 0;
-  end
-  always @(posedge beginTest) begin
-    #10;
-    
+
     //test case 1: make sure data not written when regWE = 0
-    #5 clk=1; #5 clk=0;
     regWE = 0;
     addr = 10'd1; 
     dataIn = 32'd1;
+    #5 clk=1; #5 clk=0;
     if(dataOut == dataIn) begin
+      failed = 1;
       $display("dataMemory failure: data present when regWE = 0");
     end
 
-    //#100;
-
     //test case 2
-    #5 clk=1; #5 clk=0;
     regWE = 1;
     addr = 10'd1; 
     dataIn = 32'd1;
+    #5 clk=1; #5 clk=0;
     if(dataOut != dataIn) begin
-      $display("dataMemory failure: data not present when regWE = 1");
+      failed = 1;
+      $display("dataMemory failure: data not present or not valid when regWE = 1");
     end
-
-    //#100
 
     //test case 3
+    regWE = 0;
+    addr = 10'd2; 
     #5 clk=1; #5 clk=0;
-    regWE = 1;
-    addr = 10'd1; 
-    dataIn = 32'd1;
-    if(dataOut == dataIn[10'd0]) begin
+    if(dataOut != 32'd0) begin
+      failed = 1;
       $display("dataMemory failure: data present in empty address");
     end
+
+    if (!failed) $display("dataMemory Passed");
   end
 endmodule
