b31f03f50cd93003c7c22dd7a8e7b72b448015c3
Ben Kahle <ben@kahle.org>
2014-12-03 22:20:25 -0500

    add decoder do file and fix compile errors of .v file.

--- CPU/instructionDecoder.v
+++ CPU/instructionDecoder.v
@@ -1,13 +1,13 @@
-module instructionDecoder(clk, instruction, pcSrc, regDst, regWrEn, extSel, aluSrcB, aluCommand, memWrEn, memOutSrc, rs, rt, rd, imm);
-  input clk;
+module instructionDecoder(clk, instruction, pcSrc, regDst, regWrEn, extSel, aluSrcB, aluCommand, memWrEn, memOutSrc, rs, rt, rd, imm, aluZero);
+  input clk, aluZero;
   input[31:0] instruction;
-  output[15:0] imm;
-  output[4:0] rs, rt, rd;
-  output[2:0] aluCommand; //TODO: do we need to support all 7 operations? 
-  output[1:0] pcSrc, memOutSrc;
-  output regDst, regWrEn, memWrEn, aluSrcB, extSel;
+  output reg[15:0] imm;
+  output reg[4:0] rs, rt, rd;
+  output reg[2:0] aluCommand; //TODO: do we need to support all 7 operations? 
+  output reg[1:0] pcSrc, memOutSrc;
+  output reg regDst, regWrEn, memWrEn, aluSrcB, extSel;
 
-  always @(posedge clk)
+  always @(posedge clk) begin
     // Set defaults
     rs = 5'd0;
     rt = 5'd0;
@@ -47,25 +47,29 @@ module instructionDecoder(clk, instruction, pcSrc, regDst, regWrEn, extSel, aluS
       end
       // J-Type Instructions
       6'd2: begin // j
-        pcSrc = 2'd2 // J Absolute
+        pcSrc = 2'd2; // J Absolute
       end
 
       6'd3: begin // jal
         rd = 5'd31; // Special register for $ra
         regWrEn = 1; // Write back enable
         memOutSrc = 2'd2; // PC+4
-        pcSrc = 2'd2 // J Absolute
+        pcSrc = 2'd2; // J Absolute
       end
 
       // I-Type Instructions
-      6'd4: begin // beq TODO: determine conditional
-        pcSrc = 2'd3 // br (dependent on result of conditional?)
-        
+      6'd4: begin // beq
+        aluCommand = 3'd2;
+        if (aluZero) begin
+          pcSrc = 2'd3; // br
+        end 
       end
 
-      6'd5: begin // bne TODO
-        pcSrc = 2'd3 // br (dependent on result of conditional?)
-
+      6'd5: begin // bne
+        aluCommand = 3'd2;
+        if (!aluZero) begin
+          pcSrc = 2'd3; // br (dependent on result of conditional?)
+        end
       end
 
       6'd8: begin // addi
@@ -77,7 +81,7 @@ module instructionDecoder(clk, instruction, pcSrc, regDst, regWrEn, extSel, aluS
       6'd9: begin // addiu
         // rt = rs + uSE(Imm)
         extSel = 1; // Unsigned extension
-        regDst = 1 // rt
+        regDst = 1; // rt
         regWrEn = 1; // Write back enable
       end
 
@@ -85,8 +89,8 @@ module instructionDecoder(clk, instruction, pcSrc, regDst, regWrEn, extSel, aluS
         // rt = rs < SE(Imm)
         aluSrcB = 1; // Use immediate in ALU calc
         aluCommand = 3'd3; // ALU SLT
-        regDst = 1 // rt
-        regWrEn = 1 // Write back enable
+        regDst = 1; // rt
+        regWrEn = 1; // Write back enable
       end
 
       6'd35: begin // lw
@@ -98,11 +102,12 @@ module instructionDecoder(clk, instruction, pcSrc, regDst, regWrEn, extSel, aluS
 
       6'd43: begin // sw
         // mem[rs+imm]:4 = rt
-        aluSrcB = 1 // Use immediate in ALU calc (displacement)
+        aluSrcB = 1; // Use immediate in ALU calc (displacement)
       end
     endcase
+  end
 
 endmodule
 
-module instructionDecoderTest();
+module testBench;
 endmodule
